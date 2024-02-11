003fef878156c7dfeee90b40fe62d3e13ba26570
andero2003 <andero2003@gmail.com>
2024-02-11 21:53:57 +0000

    Formatting

--- server/client/index.js
+++ server/client/index.js
@@ -1,15 +1,38 @@
 async function fetchWorkouts() {
     const workouts = await fetch(
-        'http://localhost:8080/workout/',
+        '/workout/',
     );
     console.log(workouts);
     const data = await workouts.json();
 
     console.log(data);
 
-    const elem = document.createElement('p');
-    elem.textContent = JSON.stringify(data);
-    document.body.appendChild(elem);
+    const workoutsList = document.querySelector('#workouts');
+    for (const workout of data) {
+        const workoutDiv = document.createElement('div');
+
+        const title = document.createElement('h2');
+        title.textContent = workout.name;
+        workoutDiv.append(title)
+
+        const description = document.createElement('p');
+        description.textContent = workout.description ? workout.description : 'No description';
+        workoutDiv.append(description);
+
+        workoutsList.append(workoutDiv);
+
+        const activities = await fetch(
+            `/workout/${workout.id}`,
+        );
+        const activitiesData = await activities.json();
+        const activitiesList = document.createElement('ul');
+        for (const activity of activitiesData.activities) {
+            const activityItem = document.createElement('li');
+            activityItem.textContent = `${activity.name} - ${activity.description} - ${activity.duration} sec`;
+            activitiesList.append(activityItem);
+        }
+        workoutDiv.append(activitiesList);
+    }
 }
 
 fetchWorkouts();
